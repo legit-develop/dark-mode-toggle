003ddbd460b4c93e0f8e9f1a403a1b2b934dccb2
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2022-09-21 17:12:10 +0200

    add local storage function

--- index.js
+++ index.js
@@ -3,18 +3,23 @@ const inputEl = document.querySelector(".input");
 console.log(inputEl.checked);
 
 const bodyEl = document.querySelector("body");
-inputEl.checked = false;
+inputEl.checked = JSON.parse(localStorage.getItem("mode"));
 
 updateBody();
 
 function updateBody() {
   if (inputEl.checked) {
-    bodyEl.style.background = "black";
+    bodyEl.style.background = "#000";
   } else {
-    bodyEl.style.background = "white";
+    bodyEl.style.background = "#fff";
   }
 }
 
 inputEl.addEventListener("input", () => {
   updateBody();
+  updateLocalStorage();
 });
+
+function updateLocalStorage() {
+  localStorage.setItem("mode", JSON.stringify(inputEl.checked));
+}
